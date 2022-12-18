2d47f46c3d99465b0d54f2ab372bc083ad9e6fcd
Lina Sabik <lsabik@e1r3p14.1337.ma>
2022-12-18 21:52:51 +0100

    pss

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 NAME = push_swap
 
-SRCS = ckeck_sort.c free.c main.c parsing.c push_swap.c rotate.c utilis.c utilis_1.c
+SRCS = ckeck_sort.c free.c main.c parsing.c push_swap.c rotate.c utilis.c utilis_1.c sort_3num.c
 
 OBJS = $(SRCS:.c=.o)
 
--- sort_3num.c
+++ sort_3num.c
@@ -5,9 +5,13 @@ void sort_3num(t_stack	*stacks)
 	t_stack_list	*tmp;
 
 	tmp = stacks->A;
-	if (tmp->next->content > tmp->next->next->content && tmp->content < tmp->next->next->content)
+    if (check_sort(stacks->A))
+        return ;
+	if (tmp->content > tmp->next->content)
 		{
 			swap_stack(stacks, 'a');
-			rotate(stacks,'a');
 		}
+    if ()
+			rotate(stacks,'a');
 }
+//tmp->next->content > tmp->next->next->content && tmp->content < tmp->next->next->content
